7e5617a24b369a1a39a82310e8d09c788d0af9c3
yuyuyu2118 <yuyuyu2118@gmail.com>
2023-06-21 16:25:28 +0900

    start

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import SideBar from "./components/SideBar";
+import { OverviewBudget } from './section/overview/overview-budget';
+
+function App() {
+  return (
+    <div className="App">
+      <SideBar />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/SideBar.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Button from "@mui/material/Button";
+import "./SideBar.css";
+
+const SideBar = () => {
+  return (
+    <div>
+      <Button variant="contained" className="sidebar-button" color="primary">
+        Hello
+      </Button>
+    </div>
+  );
+};
+
+export default SideBar;
